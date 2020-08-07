bac8a8c6895e413adfa63095c7b87336d9c31fe7
Potturi <potturi.k@gmail.com>
2020-08-07 22:49:44 +1000

    Added Leap Year Function

--- projectMain.py
+++ projectMain.py
@@ -12,6 +12,15 @@ def greet(who_to_greet):
     greeting = "Hello, {}".format(who_to_greet)
     return greeting
 
+"""
+Given the year and you have to write a function to check if the year is leap or not. In the Gregorian calendar three criteria must be taken into account to identify leap years:
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+"""
+
+def is_leap(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 
 def printo():
     print(greet("Murali"))
